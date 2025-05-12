6592fd7f68ad4d08e7adbdfee42beb7c3e9c9866
dev <deveshindia91@gmail.com>
2025-05-12 22:51:50 +0530

    Thread Example 1 Just created a Thread that do Nothing.

---
+++ src/main/java/org/example/level_1/ThreadExample1.java
@@ -0,0 +1,10 @@
+package org.example.level_1;
+
+public class ThreadExample1 {
+
+    // In This example we are creating a that do nothing.
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+        thread.start();
+    }
+}
